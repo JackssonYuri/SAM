a13c166dd7ebca53fd447894350eb008b6562a2e
Matheus Portela <108295503+mpm-10@users.noreply.github.com>
2024-07-01 20:00:08 -0300

    Alteração e Reescrita de Código
    Código reescrito e função de entrada e saída deslocada para outro arquivo.

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-from sys import *
-from os import *
+
 
 from compiler.compiler import Compiler
 
-examples_path : str = 'examples/input'
+compiler : Compiler = Compiler()
 
-filenames : list = []
-
-compiler : Compiler
-
-
-
-if __name__ == '__main__':
-
-    if len(argv) == 1:
-
-        for path in listdir(examples_path):
-
-            filenames.append(path)
-
-    
-    elif len(argv) > 1:
-
-        filenames = argv.copy()
-
-        filenames.pop()
-
-
-    
-    compiler = Compiler(filenames)
+if __name__ == '__main__':    
 
     compiler.compile()
--- test.py
+++ test.py
@@ -1,30 +1,11 @@
-from sys import *
-from os import *
+
 
 from compiler.compiler import Compiler
 
+compiler : Compiler = Compiler()
 
-
-examples_path : str = 'examples/input'
-
-filenames : list = []
-
-compiler : Compiler
-
-
-
-if __name__ == '__main__':
-
-    for path in listdir(examples_path):
-
-        filenames.append(path)
-
-    
-    compiler = Compiler(filenames)
+if __name__ == '__main__':    
 
     compiler.compile()
 
-    
-    
 
-    
